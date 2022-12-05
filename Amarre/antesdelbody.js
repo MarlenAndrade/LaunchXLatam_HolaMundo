ddeaa9469f8d38c756785ae8453bda40d183dd4d
MarlenAndrade <93796896+MarlenAndrade@users.noreply.github.com>
2022-12-05 16:25:55 -0600

    Delete Amarre directory

--- Amarre/antesdelbody.js
+++
@@ -1,8 +0,0 @@
-window.onclick = function(){
-    //Me crea una serie de elements por cada tag encontrado
-    let encabezados = document.createElement("h1");
-    const text = document.createTextNode("¿Quieres ser mi morrito?");
-
-    encabezados.appendChild(text);
-    document.body.appendChild(encabezados);
-}
